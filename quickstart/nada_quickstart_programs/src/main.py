349109867db016958f56328e2ceb325eae9b8a7c
rasuib12 <1032220196@tcetmumbai.in>
2024-07-04 18:01:21 +0000

    my new nada program

--- quickstart/nada_quickstart_programs/src/main.py
+++ quickstart/nada_quickstart_programs/src/main.py
@@ -1,16 +1,41 @@
 from nada_dsl import *
 
+
 def nada_main():
-    party1 = Party(name="Party1")
-    my_int1 = SecretInteger(Input(name="my_int1", party=party1))
-    my_int2 = SecretInteger(Input(name="my_int2", party=party1))
+    devansh = Party(name="Devansh")  # party 0
+    prathamesh = Party(name="Prathamesh")  # party 1
+    abhishekh = Party(name="Abhishekh")  # party 2
 
-    # Compute the sum and product of my_int1 and my_int2
-    my_sum = my_int1 + my_int2
-    my_product = my_int1 * my_int2
+    devansh_salary = SecretInteger(Input(name="devansh_salary", party=devansh))
+    prathamesh_salary = SecretInteger(Input(name="prathamesh_salary", party=prathamesh))
+    abhishekh_salary = SecretInteger(Input(name="abhishekh_salary", party=abhishekh))
 
-    # Output the sum and product
-    return [
-        Output(my_sum, "my_sum_output", party1),
-        Output(my_product, "my_product_output", party1)
-    ]
+    # Determine the lowest position
+    lowest_position = (devansh_salary < prathamesh_salary).if_else(
+        (devansh_salary < abhishekh_salary).if_else(Integer(0), Integer(2)),
+        (prathamesh_salary < abhishekh_salary).if_else(Integer(1), Integer(2)),
+    )
+
+    # Adding complexity: Check for ties
+    devansh_prathamesh_tie = (devansh_salary == prathamesh_salary)
+    prathamesh_abhishekh_tie = (prathamesh_salary == abhishekh_salary)
+    devansh_abhishekh_tie = (devansh_salary == abhishekh_salary)
+
+    # Create a more complex condition with tie checks
+    complex_lowest_position = devansh_prathamesh_tie.if_else(
+        prathamesh_abhishekh_tie.if_else(
+            Integer(0),  # All three salaries are equal
+            (devansh_salary < abhishekh_salary).if_else(Integer(0), Integer(2))  # Devansh and Prathamesh are equal, compare with Abhishekh
+        ),
+        prathamesh_abhishekh_tie.if_else(
+            (prathamesh_salary < devansh_salary).if_else(Integer(1), Integer(0)),  # Prathamesh and Abhishekh are equal, compare with Devansh
+            devansh_abhishekh_tie.if_else(
+                (devansh_salary < prathamesh_salary).if_else(Integer(0), Integer(1)),  # Devansh and Abhishekh are equal, compare with Prathamesh
+                lowest_position  # No ties, use the original lowest position
+            )
+        )
+    )
+
+    out = Output(complex_lowest_position, "lowest_position", devansh)
+
+    return [out]
