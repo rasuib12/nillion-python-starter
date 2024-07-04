f2b7a8df8f14f341f970692ceb94f35dd38ba2e3
rasuib12 <1032220196@tcetmumbai.in>
2024-07-04 10:18:16 +0000

    my new nada program

---
+++ quickstart/nada_quickstart_programs/src/main.py
@@ -0,0 +1,16 @@
+from nada_dsl import *
+
+def nada_main():
+    party1 = Party(name="Party1")
+    my_int1 = SecretInteger(Input(name="my_int1", party=party1))
+    my_int2 = SecretInteger(Input(name="my_int2", party=party1))
+
+    # Compute the sum and product of my_int1 and my_int2
+    my_sum = my_int1 + my_int2
+    my_product = my_int1 * my_int2
+
+    # Output the sum and product
+    return [
+        Output(my_sum, "my_sum_output", party1),
+        Output(my_product, "my_product_output", party1)
+    ]
